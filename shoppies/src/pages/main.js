05d918a6e476a22bed2a6c4e9817e70553eed014
Joseph Malovic <57027440+JosephDrazen@users.noreply.github.com>
2021-01-07 20:01:52 -0400

    Added: more support for search

--- shoppies/src/pages/main.js
+++ shoppies/src/pages/main.js
@@ -5,7 +5,7 @@ import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
-import {Icon, Autocomplete, Button, Avatar, TextStyle, Card, ResourceItem, ResourceList} from '@shopify/polaris';
+import {Icon, Autocomplete, Button, TextStyle, Card, ResourceItem} from '@shopify/polaris';
 import {SearchMinor} from '@shopify/polaris-icons';
 import "@shopify/polaris/dist/styles.css";
 import '../App.css';
@@ -25,14 +25,25 @@ export default function Main() {
     const classes = useStyles();
     const API_KEY = "http://www.omdbapi.com/?i=tt3896198&apikey=47eacc18";
     const [textFieldValue, setTextFieldValue] = useState('');
-    const [results, setResults] = useState();
+    const [results, setResults] = useState('');
+    const [nominations, setNominations] = useState([]);
     const [searchTerm, setSearchTerm] = useState('N/A');
 
-    const handleSearch = () => {
+    const fetchResults = useCallback(async() => {
+        let url = API_KEY + `&s=${textFieldValue}`;
+        const response = await fetch(url);
+        const body = await response.json();
+        return body;
+    }, [textFieldValue])
+
+
+    const handleSearch = useCallback(() => {
         setSearchTerm(textFieldValue);
         fetchResults()
-        .then(res => setResults(res['Search'], console.log(results)))
-    }
+        .then(res => setResults(res['Search'],
+        console.log(results)
+        ))
+    }, [fetchResults, results, setSearchTerm, textFieldValue])
 
     const handleTextFieldChange = useCallback(
         (value) => setTextFieldValue(value),
@@ -41,22 +52,30 @@ export default function Main() {
 
     const handleClearButtonClick = useCallback(() => setTextFieldValue(''), []);
 
-    const fetchResults = async() => {
-        let url = API_KEY + `&s=${textFieldValue}`;
-        const response = await fetch(url);
-        const body = await response.json();
-        return body;
-    }
+
 
     const searchBtn = () => {
         return (
             <Button
             size='large'
             primary
-            onClick={() => handleSearch()}>
+            onClick={handleSearch}>
                 Search
             </Button>
         )
+    }
+
+    const handleNominate = (movie) => {
+        if(nominations.length < 5 && !nominations.includes(movie)) {
+            let data = nominations;
+            data.push([movie])
+            setNominations(data)
+            console.log(movie)
+            console.log(nominations)
+        }
+        else {
+            console.log("FOO")
+        }
     }
 
     const textField = (
@@ -71,6 +90,10 @@ export default function Main() {
         />
     );
 
+    useEffect(() => {
+        fetchResults();
+    }, [results, fetchResults])
+
     const list = () => {
 
         if(searchTerm === 'N/A') {
@@ -79,7 +102,7 @@ export default function Main() {
             )           
         }
 
-        if(results) {
+        else if(results) {
 
             return (
                 results.map(data => (
@@ -87,17 +110,20 @@ export default function Main() {
                     style={{listStyle: 'none'}}>
                     <ResourceItem>
                         <Grid container>
-                        <Grid item xs={2}>
-                        <img width='50'
-                        style={{display: 'inline-block',
-                        boxShadow: '0px 0px 2px #888'}}
-                        src={data.Poster}></img>
-                        </Grid>
-                        <Grid item xs={6}>
-                        <h3>
-                            <TextStyle variation="strong">{data.Title} ({data.Year})</TextStyle>
-                        </h3>
-                        </Grid>
+                            <Grid item xs={2}>
+                            <img alt={data.Title} width='50'
+                            style={{display: 'inline-block',
+                            boxShadow: '0px 0px 2px #888'}}
+                            src={data.Poster}></img>
+                            </Grid>
+                            <Grid item xs={6}>
+                            <h3>
+                                <TextStyle variation="strong">{data.Title} ({data.Year})</TextStyle>
+                            </h3>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Button id={data.imdbID} onClick={(e) => handleNominate(e)}>Nominate</Button>
+                            </Grid>
                         </Grid>
                     </ResourceItem>
 
@@ -105,12 +131,12 @@ export default function Main() {
                     
                 ))
             )
-        } else {
+        }
+        else {
             return (
                 <p> No Results Found </p>
             )
         }
-
     }
 
     const breadcrumb = (
@@ -154,7 +180,11 @@ export default function Main() {
                     <Grid 
                     className={classes.container}
                     item xs={6}>
-                        <Card sectioned>Current Nominations</Card>
+                        <Card sectioned>Current Nominations
+                        </Card>
+                        {nominations.map(data => (
+                            <p>{nominations}</p>
+                        ))}
                     </Grid>
                 </Grid>
             </Container>
